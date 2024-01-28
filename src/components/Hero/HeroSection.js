80f277a5c36c46d80e2072e08f9e0035336343fd
Krushit <krushit@pop-os.localdomain>
2024-01-28 20:31:12 +0530

    upda : hero section avatar add

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -5,17 +5,20 @@ import Image from 'next/image'
 import heroavatar from '../../assets/image/hero/heroavatar.svg'
 import herocircle from '../../assets/image/hero/herocircle.svg'
 import herostar from '../../assets/image/hero/herostar.svg'
+import developer from '../../assets/image/hero/developer.png'
 function HeroSection() {
   return (
     <div className='hero-section'>
       <Image src={herocircle} alt="circle" className='herocircle'/>
       <Image src={herostar} alt="circle" className='herostar'/>
         <Row className='hero-row'>
-            <Col className='discription' lg="6">
+            <Col className='discription order-lg-1 order-2' lg="6">
               <h3>This is your web developer </h3><h4> Krushit Dholariya</h4>
               <p className='hero-description'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
             </Col>
-            {/* <div className='hero-avatar'></div> */}
+            <Col className='hero-avatar order-lg-2 order-1' lg="6">
+              <Image src={developer} alt="hero avatar" className='img-fluid hero-avatar'/>
+            </Col>
               <Image src={heroavatar} alt='hero-avatar' className='hero-image img-fluid'/>
             
         </Row>
